82b25f6e4b1625012f8a09806545e2ff7f1e757d
brichar01 <bdvrichards@gmail.com>
2021-09-07 12:36:27 +1000

    Use emotion styled

--- src/App.js
+++ src/App.js
@@ -1,24 +1,16 @@
-import logo from './logo.svg';
-import './App.css';
+import styled from "@emotion/styled";
+
+const AppComponent = styled.div({
+    textAlign: "center",
+    backgroundColor: "#bdf5f2",
+    width: "100%",
+})
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <AppComponent>
+      Something
+    </AppComponent>
   );
 }
 
